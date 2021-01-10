385dd5604527f7a9419eeb37b7304b4f6c6f42c1
wibus-wee <1596355173@qq.com>
2021-01-10 23:09:32 +0800

    Update main.yml

---
+++ build_push.sh
@@ -0,0 +1,63 @@
+# install rimraf
+sudo npm install rimraf -g
+
+# Kami clone and fix
+echo "Kami"
+git clone https://github.com/mx-space/kami
+cd kami
+# fix user files
+sudo  rm -rf .env
+sudo  rm -rf configs.ts
+sudo  wget http://api.iucky.cn/themes/kami/.env
+sudo  wget http://api.iucky.cn/themes/kami/configs.ts
+cd public
+sudo  rm -rf manifest.json
+sudo  wget http://api.iucky.cn/themes/kami/manifest.json
+cd ..
+echo "fix user files success"
+# build
+sudo yarn
+sudo yarn build
+sudo tar cf kami.tar.gz *
+sudo mv kami.tar.gz .. #此时kami已经移动到根文件夹
+cd .. # exit to root dir
+
+# Server clone and fix
+echo "Server"
+git clone https://github.com/mx-space/server
+cd server
+# fix user files
+sudo rm -rf .env
+sudo wget http://api.iucky.cn/themes/.env_server
+sudo mv .env_server .env
+echo "fix user files success"
+# build
+sudo yarn
+sudo yarn build
+sudo tar cf server.tar.gz *
+sudo mv server.tar.gz .. #移动已编译文件
+cd .. # exit to root dir
+
+# admin clone and fix
+echo "Admin"
+git clone https://github.com/mx-space/admin
+cd admin
+# fix user files
+sudo rm -rf .env
+sudo rm -rf vue.config.js
+sudo wget http://api.iucky.cn/themes/admin/.env
+sudo wget http://api.iucky.cn/themes/admin/vue.config.js
+echo "fix user files success"
+# build
+sudo yarn
+sudo yarn build
+cd dist
+sudo tar cf admin.tar.gz *
+sudo mv admin.tar.gz .. # 移动编译dist
+cd .. # exit to root dir
+
+# delete files
+echo "delete files"
+sudo rm -rf server
+sudo rm -rf kami
+sudo rm -rf admin
