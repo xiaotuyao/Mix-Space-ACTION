a5af2ea629f3bb6ecb7cf9f96f40187938ab653c
wibus-wee <1596355173@qq.com>
2021-01-30 16:26:30 +0800

    🥳 实际环境可用方式

---
+++ admin/vue.config.js
@@ -0,0 +1,77 @@
+/*
+ * @Author: Innei
+ * @Date: 2020-05-27 19:10:07
+ * @LastEditTime: 2020-05-30 12:45:15
+ * @LastEditors: Innei
+ * @FilePath: /mx-admin/vue.config.js
+ * @Code with Love
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-use-before-define */
+'use strict'
+// const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require('webpack')
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following methods:
+// port = 9528 npm run dev OR npm run dev --port = 9528
+const port = process.env.port || process.env.npm_config_port || 9528 // dev port
+
+// All configuration item explanations can be find in https://cli.vuejs.org/config/
+module.exports = {
+  /**
+   * You will need to set publicPath if you plan to deploy your site under a sub path,
+   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+   * then publicPath should be set to "/bar/".
+   * In most cases please use '/' !!!
+   * Detail: https://cli.vuejs.org/config/#publicpath
+   */
+  outputDir: 'dist',
+  assetsDir: 'static',
+  productionSourceMap: false,
+  pwa: {
+    name: 'mx-space-admin',
+    themeColor: '#1681e1',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  },
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+  },
+
+  configureWebpack: (config) => {
+    config.plugins.push(
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/),
+    )
+  },
+  chainWebpack: (config) => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach((type) =>
+      addStyleResource(config.module.rule('scss').oneOf(type)),
+    )
+  },
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: process.env.NODE_ENV === 'production' ? true : false,
+    },
+  },
+}
+
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        require('path').resolve(__dirname, 'src/assets/scss/_var.scss'),
+      ],
+    })
+}
---
+++ kami/configs.ts
@@ -0,0 +1,122 @@
+import { faGithub, faQq, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faCircle, faDotCircle } from '@fortawesome/free-regular-svg-icons'
+import {
+  faBookOpen,
+  faComments,
+  faFeatherAlt,
+  faGlasses,
+  faHistory,
+  faUserFriends,
+} from '@fortawesome/free-solid-svg-icons'
+import { MenuModel, SocialLinkModel } from 'common/store/types'
+const menu: MenuModel[] = [
+  {
+    title: '源',
+    path: '/',
+    type: 'Home',
+    icon: faDotCircle,
+    subMenu: [],
+  },
+  {
+    title: '文',
+    path: '/posts',
+    type: 'Post',
+    subMenu: [],
+    icon: faGlasses,
+  },
+  {
+    title: '记',
+    type: 'Note',
+    path: '/notes',
+    icon: faFeatherAlt,
+  },
+  {
+    title: '言',
+    path: '/says',
+    icon: faComments,
+  },
+  {
+    title: '览',
+    icon: faHistory,
+    path: '/timeline',
+    subMenu: [
+      {
+        title: '生活',
+        icon: faFeatherAlt,
+        path: '/timeline?type=note',
+      },
+      {
+        title: '博文',
+        icon: faBookOpen,
+        path: '/timeline?type=post',
+      },
+    ],
+  },
+  {
+    title: '友',
+    icon: faUserFriends,
+    path: '/friends',
+  },
+  // {
+  //   title: '趣',
+  //   icon: faCircleNotch,
+  //   path: '/favorite/music',
+  //   subMenu: [
+  //     {
+  //       title: '音乐',
+  //       icon: faMusic,
+  //       type: 'Music',
+  //       path: '/favorite/music',
+  //     },
+  //     {
+  //       title: '追番',
+  //       icon: faTv,
+  //       type: 'Bangumi',
+  //       path: '/favorite/bangumi',
+  //     },
+  //     {
+  //       title: '项目',
+  //       icon: faFlask,
+  //       type: 'Project',
+  //       path: '/projects',
+  //     },
+  //   ],
+  // },
+]
+const social: SocialLinkModel[] = [
+  {
+    url: 'https://github.com/wibus-wee',
+    title: 'GitHub',
+    icon: faGithub,
+    color: 'var(--black)',
+  },
+  {
+    url: 'https://jq.qq.com/?_wv=1027&k=iZxOGzDW',
+    title: 'QQ',
+    icon: faQq,
+    color: '#12b7f5',
+  },
+  {
+    url: '#',
+    title: 'twitter',
+    icon: faTwitter,
+    color: '#02A4ED',
+  },
+]
+export default {
+  url: 'https://iucky.cn',
+  alwaysHTTPS:
+    process.env.NODE_ENV === 'development'
+      ? false
+      : process.env.ALWAYS_HTTPS && parseInt(process.env.ALWAYS_HTTPS) === 1,
+  social,
+  biliId: 26578164,
+  homePage: 'https://iucky.cn', // footer link
+  menu,
+  icp: {
+    name: '#',
+    url: '#',
+  },
+  travellings: true, // 开往
+  donate: '#',
+}
